518a5c9c5ed4e891198f0c0e217d8d4795301d98
Aether <aether.wu@gmail.com>
2020-05-02 20:52:14 +0800

    ScanPaging

--- pagination/pagination.go
+++ pagination/pagination.go
@@ -84,6 +84,63 @@ func Paging(p *Param, result interface{}) *Paginator {
 	return &paginator
 }
 
+// Paging for scanning struct 分页
+func ScanPaging(p *Param, result interface{}) *Paginator {
+	db := p.DB
+
+	if p.ShowSQL {
+		db = db.Debug()
+	}
+	if p.Page < 1 {
+		p.Page = 1
+	}
+	if p.Limit == 0 {
+		p.Limit = 10
+	}
+	if len(p.OrderBy) > 0 {
+		for _, o := range p.OrderBy {
+			db = db.Order(o)
+		}
+	}
+
+	done := make(chan bool, 1)
+	var paginator Paginator
+	var count int
+	var offset int
+
+	go countRecords(db, result, done, &count)
+
+	if p.Page == 1 {
+		offset = 0
+	} else {
+		offset = (p.Page - 1) * p.Limit
+	}
+
+	db.Limit(p.Limit).Offset(offset).Scan(result)
+	<-done
+
+	paginator.TotalRecord = count
+	paginator.Records = result
+	paginator.Page = p.Page
+
+	paginator.Offset = offset
+	paginator.Limit = p.Limit
+	paginator.TotalPage = int(math.Ceil(float64(count) / float64(p.Limit)))
+
+	if p.Page > 1 {
+		paginator.PrevPage = p.Page - 1
+	} else {
+		paginator.PrevPage = p.Page
+	}
+
+	if p.Page == paginator.TotalPage {
+		paginator.NextPage = p.Page
+	} else {
+		paginator.NextPage = p.Page + 1
+	}
+	return &paginator
+}
+
 func countRecords(db *gorm.DB, anyType interface{}, done chan bool, count *int) {
 	db.Model(anyType).Count(count)
 	done <- true
